a409c93c00c667c8f3cc6ae3c89d132ac489e037
Your Name <itsdavidwu@gmail.com>
2014-06-29 22:47:42 -0400

    chapter 6, adding user model

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,5 +13,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+
+  config.use_transactional_fixtures = true
+  
   config.include Capybara::DSL
 end
