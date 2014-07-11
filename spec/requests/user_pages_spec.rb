fca0d609ea3f45eaa90677dd209227756919109c
Your Name <itsdavidwu@gmail.com>
2014-07-10 23:36:38 -0400

    Finished chapter 7 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,10 +17,20 @@ describe "UserPages" do
     let(:submit) { "Create my account"}
 
     describe "with invalid information" do
-      it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+
+      describe "after submission" do 
+        before {click_button submit}
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+        it { should have_content('Name can\'t be blank') }
+        it { should have_content('Email can\'t be blank') }
       end
     end
+
 
     describe "with valid information" do
       before do
@@ -31,10 +41,17 @@ describe "UserPages" do
       end
 
       it "should create a user" do
-        expect { click_button submit}.to change(User, :count).by(1)
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving a user" do 
+        before {click_button submit}
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
-  end
 
   	describe "profile page" do
   		let(:user) { FactoryGirl.create(:user) }
@@ -43,4 +60,5 @@ describe "UserPages" do
   		it { should have_content(user.name) }
   		it { should have_title(user.name) }
   	end
+  end
 end
