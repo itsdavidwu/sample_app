ddb099df518aa5cd2b18c913f659a342aa708a0c
Your Name <itsdavidwu@gmail.com>
2014-08-17 19:39:20 -0700

    adding microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -39,4 +39,11 @@ module SessionsHelper
   	!current_user.nil?
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
 end
